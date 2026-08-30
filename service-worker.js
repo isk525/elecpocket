7b18aedfcb8164d82a00ae19cf9dee1514d86eec
isk525 <y.isk.1108@gmail.com>
2026-08-31 01:50:43 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'elecpocket-v1';
+const APP_SHELL = [
+  '/elecpocket/',
+  '/elecpocket/index.html',
+  '/elecpocket/manifest.webmanifest',
+  '/elecpocket/elecpocket192.png',
+  '/elecpocket/elecpocket512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key.startsWith('elecpocket-') && key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('/elecpocket/index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('/elecpocket/index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
